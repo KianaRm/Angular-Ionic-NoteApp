706098031fdbcd2871db7a19496c5420f71d76ce
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2025-11-13 23:51:41 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyB49OtAJbH4nWA2fkbbH_8ooJgJU7V5UHI",
+    authDomain: "noteappproject-d8b01.firebaseapp.com",
+    projectId: "noteappproject-d8b01",
+    storageBucket: "noteappproject-d8b01.appspot.com",
+    messagingSenderId: "1012404412600",
+    appId: "1:1012404412600:web:9f45a31c9cdcab9f00944d"
+  }
 };
